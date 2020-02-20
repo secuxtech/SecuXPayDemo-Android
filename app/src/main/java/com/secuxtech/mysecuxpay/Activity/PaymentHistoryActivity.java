1e4d5d5e5223b2a3f2dc324cb5dba1f12eebbcd7
maochunsun <maochuns.sun@gmail.com>
2020-02-20 18:46:06 +0800

    Update PaymentHistoryActivity.java

--- app/src/main/java/com/secuxtech/mysecuxpay/Activity/PaymentHistoryActivity.java
+++ app/src/main/java/com/secuxtech/mysecuxpay/Activity/PaymentHistoryActivity.java
@@ -2,21 +2,31 @@ package com.secuxtech.mysecuxpay.Activity;
 
 import android.app.ActionBar;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.core.util.Pair;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.secuxtech.mysecuxpay.Adapter.HistoryListAdapter;
-import com.secuxtech.mysecuxpay.Model.Wallet;
+
+import com.secuxtech.mysecuxpay.Interface.OnListScrollListener;
+import com.secuxtech.mysecuxpay.Model.Setting;
 import com.secuxtech.mysecuxpay.R;
+import com.secuxtech.paymentkit.SecuXPaymentHistory;
+import com.secuxtech.paymentkit.SecuXPaymentManager;
+
+import java.util.ArrayList;
 
 public class PaymentHistoryActivity extends BaseActivity {
 
     HistoryListAdapter mAdapter;
+    SecuXPaymentManager mPaymentManager = new SecuXPaymentManager();
+    ArrayList<SecuXPaymentHistory> payHisArr = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +34,53 @@ public class PaymentHistoryActivity extends BaseActivity {
         setContentView(R.layout.activity_payment_history);
 
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView_history_list);
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView_history_list);
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL));
 
-        if (Wallet.getInstance().getPaymentHistory().size()>0){
-            mAdapter = new HistoryListAdapter(this, Wallet.getInstance().getPaymentHistory());
-            recyclerView.setAdapter(mAdapter);
-        }else{
-            TextView textviewNoHistory = findViewById(R.id.textView_nohistory);
-            textviewNoHistory.setVisibility(View.VISIBLE);
-            recyclerView.setVisibility(View.VISIBLE);
-        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                Pair<Boolean, String> ret = mPaymentManager.getPaymentHistory(Setting.getInstance().mAccount, "SPC", 1, 20, payHisArr);
+                if (!ret.first){
+                    showMessageInMain("Get payment history failed! Error: " + ret.second);
+
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (payHisArr.size()>0){
+                            mAdapter = new HistoryListAdapter(mContext, payHisArr);
+                            recyclerView.setAdapter(mAdapter);
+
+                            mAdapter.setOnListScrollListener(new OnListScrollListener(){
+                                @Override
+                                public void onBottomReached(int position){
+                                    Log.i(TAG, "onBottomReached");
+                                }
+
+                                @Override
+                                public void onTopReached(int position){
+                                    Log.i(TAG, "onTopReached");
+                                }
+                            });
+
+                        }else{
+                            TextView textviewNoHistory = findViewById(R.id.textView_nohistory);
+                            textviewNoHistory.setVisibility(View.VISIBLE);
+                            recyclerView.setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
+
+            }
+        }).start();
+
+
+
+
 
 
 
