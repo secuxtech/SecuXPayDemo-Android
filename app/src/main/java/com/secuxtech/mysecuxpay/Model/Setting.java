aab8041179711f7c472a55380e855b25aa5f05e8
maochunsun <maochuns.sun@gmail.com>
2020-03-30 18:50:46 +0800

    Update Setting.java

--- app/src/main/java/com/secuxtech/mysecuxpay/Model/Setting.java
+++ app/src/main/java/com/secuxtech/mysecuxpay/Model/Setting.java
@@ -1,11 +1,14 @@
 package com.secuxtech.mysecuxpay.Model;
 
+import androidx.core.util.Pair;
+
 import com.secuxtech.paymentkit.SecuXCoinAccount;
 import com.secuxtech.paymentkit.SecuXCoinTokenBalance;
 import com.secuxtech.paymentkit.SecuXPaymentHistory;
 import com.secuxtech.paymentkit.SecuXUserAccount;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -22,6 +25,7 @@ public class Setting {
     public SecuXUserAccount mAccount = null;
     public SecuXPaymentHistory mLastPaymentHis = null;
 
+    public List<Pair<String, String>> mCoinTokenArray = new ArrayList<>();
     public boolean mTestModel = false;
 
     private Setting() {
