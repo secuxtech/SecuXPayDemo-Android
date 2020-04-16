724b199fc1735591e81ff24115023a42b6fc4e5f
maochunsun <maochuns.sun@gmail.com>
2020-04-16 18:56:51 +0800

    Update Setting.java

--- app/src/main/java/com/secuxtech/mysecuxpay/Model/Setting.java
+++ app/src/main/java/com/secuxtech/mysecuxpay/Model/Setting.java
@@ -1,5 +1,8 @@
 package com.secuxtech.mysecuxpay.Model;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import androidx.core.util.Pair;
 
 import com.secuxtech.paymentkit.SecuXCoinAccount;
@@ -11,6 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by maochuns.sun@gmail.com on 2020-02-20
@@ -25,11 +30,29 @@ public class Setting {
     public SecuXUserAccount mAccount = null;
     public SecuXPaymentHistory mLastPaymentHis = null;
 
+    public String mUserAccountName = "";
+    public String mUserAccountPwd = "";
+    public boolean mUserLogout = false;
+
     public List<Pair<String, String>> mCoinTokenArray = new ArrayList<>();
     public boolean mTestModel = false;
 
     private Setting() {
+
     }
 
+    public void saveSettings(Context context){
+        SharedPreferences sharedPreferences = context.getApplicationContext().getSharedPreferences("SecuXEvPay", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("UserAccountName", mUserAccountName);
+        editor.putString("UserAccountPwd", mUserAccountPwd);
+        editor.apply();
+    }
+
+    public void loadSettings(Context context){
+        SharedPreferences settings = context.getApplicationContext().getSharedPreferences("SecuXEvPay", MODE_PRIVATE);
+        mUserAccountName = settings.getString("UserAccountName", "");
+        mUserAccountPwd = settings.getString("UserAccountPwd", "");
+    }
 
 }
